05d7fcad68d4537db958811b41eec15c10feb59e
byronkamal <byronkamal@hotmail.com>
2020-06-27 23:57:09 -0300

    Add another parameter to the corridor

--- packaging_tutorial/github_api/runner.py
+++ packaging_tutorial/github_api/runner.py
@@ -4,7 +4,9 @@ import click
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--repository',
               help='insira a url do repositorio que deseja ser analisado.')
-def start(count, repository):
+@click.option('--name',
+              help='Type your name')
+def start(count, repository, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
-        click.echo('Hello %s!' % repository)
+        click.echo('Hello %s %s' % (repository, name))
