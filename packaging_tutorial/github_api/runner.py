85aee4ed8a1fb3393494fa16b3812017bb5196d6
byronkamal <byronkamal@hotmail.com>
2020-06-30 23:57:53 -0300

    Initial github library test

--- packaging_tutorial/github_api/runner.py
+++ packaging_tutorial/github_api/runner.py
@@ -1,12 +1,25 @@
 import click
+from github import Github
+
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
 @click.option('--repository',
               help='insira a url do repositorio que deseja ser analisado.')
-@click.option('--name',
-              help='Type your name')
-def start(count, repository, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s %s' % (repository, name))
+@click.option('--username',
+              help='Type your username')
+@click.option('--password',
+              help='Type your password')
+def start(repository, username, password):
+    # """Simple program that greets NAME for a total of COUNT times."""
+    # for x in range(count):
+    #     click.echo('Hello %s %s' % (repository, name))
+
+    # github_api.getRepository(username, password)
+
+    g = Github(username, password)
+
+    repo = g.get_repo(repository)
+
+    open_issues = repo.get_issues(state='open')
+    for issue in open_issues:
+        print(issue)
--- packaging_tutorial/setup.py
+++ packaging_tutorial/setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     include_package_data = True,
     install_requires=[
         'Click',
+        'PyGithub'
     ],
     entry_points='''
         [console_scripts]
